05bd1f9019e3faf468b0adcff64cd1b714eb9432
Gherghina Cristian <cristi.gherghina_1995@yahoo.com>
2018-11-20 16:46:55 +0200

    [HH-2][Database] Create databse module

---
+++ holiholic_database/src/main/java/com/holiholic/database/DatabaseManager.java
@@ -0,0 +1,122 @@
+package com.holiholic.database;
+
+import com.holiholic.database.constant.Constants;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/* DatabaseManager - Handle the requests to the database
+ *
+ */
+public class DatabaseManager {
+    private static final Logger LOGGER = Logger.getLogger(DatabaseManager.class.getName());
+
+    /* setLogger - This method should be changed when release application
+     *             Sets the logger to print to console (instead of a file)
+     *
+     *  @return             : void
+     */
+    static void setLogger() {
+        // add logger handler
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setLevel(Level.ALL);
+
+        LOGGER.addHandler(ch);
+        LOGGER.setLevel(Level.ALL);
+    }
+
+    /* getUsers - Retrieve users from database
+     *
+     *  @return             : users in the json format
+     */
+    private static JSONObject getUsers() {
+        try {
+            InputStream is = new FileInputStream(Constants.USERS_DB_PATH);
+            String text = IOUtils.toString(is, "UTF-8");
+            return new JSONObject(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /* createRecord - Create a new record for the current user to be stored in the database
+     *
+     *  @return             : record (json format)
+     *  @request            : user information
+     *  @id                 : the id (unique) for the current user
+     */
+    private static JSONObject createRecord(JSONObject request, int id) {
+        JSONObject record = new JSONObject();
+        record.put("name", request.getString("name"));
+        record.put("email", request.getString("email"));
+        record.put("imageUrl", request.getString("imageUrl"));
+        record.put("id", id);
+        return record;
+    }
+
+    /* updateUsers - Save the users in the database
+     *
+     *  @return             : true/false (success or not)
+     *  @users              : all users (json format)
+     */
+    private static boolean updateUsers(JSONObject users) {
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(Constants.USERS_DB_PATH), 32768);
+            out.write(users.toString(2));
+            out.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /* registerUser - Save the new user in the database
+     *
+     *  @return             : true/false (success or not)
+     *  @request            : the json object containing information about the new user
+     */
+    public static boolean registerUser(JSONObject request) {
+        try {
+            LOGGER.log(Level.FINE, "Started updating the users database");
+
+            synchronized (DatabaseManager.class) {
+                JSONObject users = getUsers();
+                if (users == null) {
+                    return false;
+                }
+                users.put(request.getString("md5Key"), createRecord(request, users.length()));
+                if (!updateUsers(users)) {
+                    return false;
+                }
+            }
+
+            LOGGER.log(Level.FINE, "Successfully updated the users database");
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /* containsUser - Check if the current user is already in the database
+     *
+     *  @return             : true/false (exists or not)
+     *  @md5Key             : the unique identifier for the current user
+     */
+    public static boolean containsUser(String md5Key) {
+        JSONObject users = getUsers();
+        if (users == null) {
+            return false;
+        }
+        return users.has(md5Key);
+    }
+}
---
+++ holiholic_database/src/main/java/com/holiholic/database/constant/Constants.java
@@ -0,0 +1,8 @@
+package com.holiholic.database.constant;
+
+/* Constants - All constants will be declared here
+ *
+ */
+public class Constants {
+    public final static String USERS_DB_PATH = System.getProperty("user.dir") + "/db/users/users.json";
+}
