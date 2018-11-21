65b3d7cf38746e65b97f7623846fdf13554658c1
Cristian Gherghina <cristi.gherghina_1995@yahoo.com>
2018-11-21 17:48:23 +0200

    [HH-4][FOLLOW] Create follow module
    * [HH-4][FOLLOW] Create follow module
    
    * .gitignore is now working
    
    * get and update topics in the database
    
    * get and update people (followers) in the database
    
    * Added operation parameter

---
+++ holiholic_follow/src/main/java/com/holiholic/follow/constant/Constants.java
@@ -0,0 +1,11 @@
+package com.holiholic.follow.constant;
+
+/* Constants - All constants will be declared here
+ *
+ */
+public class Constants {
+    public final static String GET_TOPICS_URL = "http://localhost:8092/getTopics?md5Key=";
+    public final static String UPDATE_TOPICS_URL = "http://localhost:8092/updateTopics";
+    public final static String GET_PEOPLE_URL = "http://localhost:8092/getPeople?md5Key=";
+    public final static String UPDATE_PEOPLE_URL = "http://localhost:8092/updatePeople";
+}
