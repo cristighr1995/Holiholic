41a7b87e3f4ff8bfca9ab18a7499a10196db6782
Roxana Macarie <roxana_macarie2000@yahoo.com>
2018-11-21 17:07:08 +0200

    created post cotroller file

---
+++ holiholic_feed/src/main/java/com/holiholic/feed/controller/PostController.java
@@ -0,0 +1,39 @@
+package com.holiholic.feed.controller;
+
+import com.holiholic.feed.database.DatabaseManager;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PostController {
+
+    @RequestMapping(value = "/updateQuestion", headers="Content-Type=application/json", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<Boolean> updateQuestion(@RequestBody String request)  {
+        boolean result;
+        try {
+            result = DatabaseManager.updateQuestion(new JSONObject(request));
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+        }
+        return new ResponseEntity<>(result, result ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
+    }
+
+    @RequestMapping(value = "/getQuestions", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<String> getQuestions(@RequestParam String city,
+                                               @RequestParam String md5Key)  {
+        return new ResponseEntity<>(DatabaseManager.getQuestions(city, md5Key), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getQuestionDetails", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<String> getQuestionDetails(@RequestParam String city,
+                                                     @RequestParam String qid,
+                                                     @RequestParam String md5Key)  {
+        return new ResponseEntity<>(DatabaseManager.getQuestionDetails(city, qid, md5Key), HttpStatus.OK);
+    }
+}
