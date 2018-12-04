859d4a18283b4ddb6b9b970dc97930cd1bc77d17
Cristian Gherghina <cristi.gherghina_1995@yahoo.com>
2018-12-04 17:11:23 +0200

    [HH-11][PLANNER] Update planner database

--- holiholic_planner/src/main/java/com/holiholic/planner/HoliholicPlannerApplication.java
+++ holiholic_planner/src/main/java/com/holiholic/planner/HoliholicPlannerApplication.java
@@ -1,6 +1,7 @@
 package com.holiholic.planner;
 
 import com.holiholic.planner.database.DatabaseManager;
+import com.holiholic.planner.update.parser.ParserManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,5 +11,6 @@ public class HoliholicPlannerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HoliholicPlannerApplication.class, args);
         DatabaseManager.setLogger();
+		ParserManager.setLogger();
 	}
 }
