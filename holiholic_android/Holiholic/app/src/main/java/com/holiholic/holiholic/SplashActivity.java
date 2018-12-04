e9869d315983b98b5e3537f1eb9111e199fb88a8
Roxana Macarie <roxana_macarie2000@yahoo.com>
2018-12-04 17:30:21 +0200

    [HH-12][ANDROID] Create module

---
+++ holiholic_android/Holiholic/app/src/main/java/com/holiholic/holiholic/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.holiholic.holiholic;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
