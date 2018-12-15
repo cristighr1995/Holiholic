7b6544c8b5a73aa028dbc4a1ce2f9edb768106ed
Gherghina Cristian <cristi.gherghina_1995@yahoo.com>
2018-12-15 16:13:36 +0200

    created UpdateAction classes to handle the reqs and cache the results

---
+++ holiholic_planner/src/main/java/com/holiholic/planner/database/UpdateAction.java
@@ -0,0 +1,40 @@
+package com.holiholic.planner.database;
+
+import com.holiholic.planner.constant.Constants;
+import org.json.JSONObject;
+
+/* UpdateAction - Handle the update planner database operations
+ *
+ */
+abstract class UpdateAction {
+
+    /* Factory - Create an instance of update action
+     *
+     */
+    static class Factory {
+        static UpdateAction getInstance(String type) {
+            switch (type) {
+                case "places":
+                    return new UpdatePlacesAction();
+                case "matrix":
+                    return new UpdateMatrixAction();
+                default:
+                    return null;
+            }
+        }
+    }
+
+    /* execute - Make the request to the database module
+     *
+     *  @return             : success or not
+     *  @body               : the request body
+     */
+    boolean execute(JSONObject body) {
+        try {
+            return DatabaseManager.postContentToURL(body, Constants.UPDATE_PLANNER_URL);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
