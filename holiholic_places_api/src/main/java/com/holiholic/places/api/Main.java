d8d89332cb2f954f1ae8722f2fa17ba8d094f184
Gherghina Cristian <cristi.gherghina_1995@yahoo.com>
2018-12-19 15:44:40 +0200

    get details of a venue and create a task for each get request

--- holiholic_places_api/src/main/java/com/holiholic/places/api/Main.java
+++ holiholic_places_api/src/main/java/com/holiholic/places/api/Main.java
@@ -1,9 +1,13 @@
 package com.holiholic.places.api;
 
+import org.json.JSONObject;
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Places.getPlaces("Bucharest", Arrays.asList("4bf58dd8d48988d181941735"));
+        // Places.getPlaces("Bucharest", Arrays.asList("4bf58dd8d48988d181941735"));
+
+        JSONObject obj = Places.buildPlaceInfo("4dfc6d43fa76c83b6e7ca607");
     }
 }
